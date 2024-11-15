8e31087893e1ad00fc7aaa2a6389d5d77b32bc00
tejeee06 <atejegom@gmail.com>
2024-11-15 13:13:17 +0100

    Diagrama de classes

---
+++ Apunts/DiagramaClase/persona.py
@@ -0,0 +1,34 @@
+class Persona:
+    def __init__(self, nom, edat):
+        self.nom = nom
+        self.edat = edat
+
+    def parlar(self ):
+        print(f"{self.nom} esta parlant.")
+
+persona_pepe = Persona(nom= 'Pepe', edat= '18')
+
+persona_pepe.parlar()
+
+class Estudiant(Persona):
+    def __init__(self, nom, edat, estudi):
+        super().__init__(nom, edat)
+        self.estudi = estudi
+
+    def estudiar(self):
+        print(f"{self.nom} està estudiant {self.estudi}.")
+
+estudiant_pepe = Estudiant(nom='Pepe', edat='18', estudi= 'ADE')
+estudiant_pepe.estudiar()
+estudiant_pepe.parlar()
+
+class Professor(Persona):
+    def __init__(self, nom, edat, sou):
+        super().__init__(nom, edat)
+        self.sou = sou
+
+    def cobrar(self ):
+        print(f"El professor {self.nom} cobra {self.sou}")
+
+professor_ignasi = Professor(nom='Igansi', edat='19', sou='4000 €')
+professor_ignasi.cobrar()
